02e040d0d84927e9feb1938fec4bfdfa4d7bae86
MarcusNerva <hadrianus_1@163.com>
2021-01-20 20:32:07 +0800

    fix: fix bugs of io.write_results()

--- utils/io.py
+++ utils/io.py
@@ -14,15 +14,15 @@ def write_results(filename, results, data_type):
         raise ValueError(data_type)
 
     with open(filename, 'w') as f:
-        for frame_id, tlwhs, track_ids in results:
+        for frame_id, tlwhs, class_idxs, track_ids in results:
             if data_type == 'kitti':
                 frame_id -= 1
-            for tlwh, track_id in zip(tlwhs, track_ids):
+            for tlwh, class_id, track_id in zip(tlwhs, class_idxs, track_ids):
                 if track_id < 0:
                     continue
                 x1, y1, w, h = tlwh
                 x2, y2 = x1 + w, y1 + h
-                line = save_format.format(frame=frame_id, id=track_id, x1=x1, y1=y1, x2=x2, y2=y2, w=w, h=h)
+                line = save_format.format(frame=frame_id, id=track_id, class_idx=class_id, x1=x1, y1=y1, x2=x2, y2=y2, w=w, h=h)
                 f.write(line)
 
 
