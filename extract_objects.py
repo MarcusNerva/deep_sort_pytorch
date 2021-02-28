a155f2853e6fa3240a372c107d8824544e8fa696
MarcusNerva <hadrianus_1@163.com>
2021-02-28 09:23:27 +0800

    fix: fixed some bugs

--- extract_objects.py
+++ extract_objects.py
@@ -15,13 +15,14 @@ obtain_tracks_cmd = 'python /home/hanhuaye/PythonProject/opensource/deep_sort_py
 mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
 normalize = transforms.Normalize(mean=mean, std=std)
 trans = transforms.Compose([
+    transforms.Resize(224),
     transforms.ToTensor(),
     normalize
 ])
 resnext_dim = 2048
 
 
-def process_pos(pos, width, height):
+def process_pos(pos, width, height, new_width, new_height):
     pos[0] = max(0, pos[0])
     pos[1] = max(0, pos[1])
     pos[2:] = pos[:2] + pos[2:]
@@ -31,6 +32,9 @@ def process_pos(pos, width, height):
     x2 = min(x2, width)
     y1 = min(y1, height)
     y2 = min(y2, height)
+
+    x_scale, y_scale = float(new_width) / width, float(new_height) / height
+    x1, y1, x2, y2 = x1 * x_scale, y1 * y_scale, x2 * x_scale, y2 * y_scale
 
     return np.array([x1, y1, x2, y2], dtype=float)
 
@@ -43,42 +47,13 @@ def get_visual_data(save_dir, data_dir, video_dir, video_name, resnext101, devic
     class_feature_dir = os.path.join(data_dir, 'category')
     visual_feature_path = os.path.join(visual_feature_dir, video_name + '.npy')
     class_feature_path = os.path.join(class_feature_dir, video_name + '.npy')
-    objects_mask_path = os.path.join(visual_feature_dir, video_name + '_mask.npy')
+    objects_mask_path = os.path.join(visual_feature_dir, video_name + '_mask.pkl')
     hastracks_path = os.path.join(visual_feature_dir, video_name + '_hastracks.pkl')
 
     with open(tracks_path, 'rb') as f:
         tracks = pickle.load(f)
     with open(length_path, 'rb') as f:
         frames_numb = pickle.load(f)
-
-    if len(tracks) == 0:
-        has_tracks = False
-        visual_features = np.zeros([15, 1, 2048, 4, 4])
-        objects_mask = np.zeros(15, 1)
-        class_features = np.zeros([1, 1000], dtype=int)
-
-        np.save(visual_feature_path, visual_features)
-        np.save(class_feature_path, class_features)
-        np.save(objects_mask_path, objects_mask)
-        with open(hastracks_path, 'wb') as f:
-            pickle.dump(has_tracks, f)
-        return
-
-    visit = np.zeros(frames_numb + 5, dtype=int)
-    frames_store = []
-
-    import cv2
-    vdo = cv2.VideoCapture()
-    vdo.open(video_path)
-    im_width, im_height = int(vdo.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vdo.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    while vdo.grab():
-        _, ori_im = vdo.retrieve()
-        im = cv2.cvtColor(ori_im, cv2.COLOR_BGR2RGB)
-        im = Image.fromarray(im)
-        frames_store.append(trans(im).unsqueeze(0).to(device))
-    assert len(frames_store) == frames_numb, \
-        'expect len(frames_store) == frames_numb, ' \
-        'but got {} and {}'.format(len(frames_store), frames_numb)
 
     interval = frames_numb // 15
     tracks = [item for item in tracks if len(item['frame_ids']) >= interval]
@@ -87,6 +62,36 @@ def get_visual_data(save_dir, data_dir, video_dir, video_name, resnext101, devic
         tracks = tracks[:20]
     num_tracks = len(tracks)
 
+    if num_tracks == 0:
+        has_tracks = False
+        visual_features = np.zeros([15, 1, 2048, 4, 4])
+        objects_mask = np.zeros([15, 1])
+        class_features = np.zeros([1, 1000], dtype=int)
+
+        np.save(visual_feature_path, visual_features)
+        np.save(class_feature_path, class_features)
+        with open(objects_mask_path, 'wb') as f:
+            pickle.dump(objects_mask, f)
+        with open(hastracks_path, 'wb') as f:
+            pickle.dump(has_tracks, f)
+        return
+
+    frames_store = []
+    import cv2
+    vdo = cv2.VideoCapture()
+    vdo.open(video_path)
+    im_width, im_height = int(vdo.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vdo.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    while vdo.grab():
+        _, ori_im = vdo.retrieve()
+        im = cv2.cvtColor(ori_im, cv2.COLOR_BGR2RGB)
+        im = Image.fromarray(im)
+        frames_store.append(trans(im).unsqueeze(0))
+    new_height, new_width = frames_store[0].shape[-2], frames_store[0].shape[-1]
+    assert len(frames_store) == frames_numb, \
+        'expect len(frames_store) == frames_numb, ' \
+        'but got {} and {}'.format(len(frames_store), frames_numb)
+
+    visit = np.zeros(frames_numb + 5, dtype=int)
     for track in tracks:
         frames_ids = track['frame_ids']
         for id in frames_ids:
@@ -98,7 +103,7 @@ def get_visual_data(save_dir, data_dir, video_dir, video_name, resnext101, devic
         class_vec = np.zeros(1000, dtype=int)
         class_vec[class_id] = 1
         class_store.append(class_vec)
-    class_store = np.concatenate([item.unsqueeze(0) for item in class_store], axis=0)
+    class_store = np.concatenate([item[np.newaxis, ...] for item in class_store], axis=0)
     assert class_store.shape == (num_tracks, 1000), \
         'expected class_store.shape == ({}, 1000), but got {}'.\
             format(num_tracks, class_store.shape)
@@ -109,11 +114,15 @@ def get_visual_data(save_dir, data_dir, video_dir, video_name, resnext101, devic
     has_tracks = True
     cnt = 0
     for i in range(frames_numb):
-        cnt += 1 if visit[i] > 0 else 0
-        if visit[i] > 0 and cnt % step == 0:
+        if visit[i] == 0: continue
+        cnt += 1
+        if cnt % step == 0:
             anchor_idxs.append(i)
+    idxs = np.linspace(0, len(anchor_idxs), time_span, endpoint=False, dtype=int).tolist()
+    anchor_idxs = [anchor_idxs[i] for i in idxs]
     assert len(anchor_idxs) == time_span, \
-        'expected len(anchor_idxs) == time_span, but got {}'.format(len(anchor_idxs))
+        'expected len(anchor_idxs) == time_span, but got {}, and now step is {}'.\
+            format(len(anchor_idxs), step)
 
     pool_size = 4
     spatial_scale = 1.0 / 32.0
@@ -124,7 +133,7 @@ def get_visual_data(save_dir, data_dir, video_dir, video_name, resnext101, devic
     objects_mask = np.zeros([15, len(tracks)])
     for i, idx in enumerate(anchor_idxs):
         temp_store = []
-        feature_map = resnext101(frames_store[idx])  # (1, 2048, H/32, W/32)
+        feature_map = resnext101(frames_store[idx].to(device))  # (1, 2048, H/32, W/32)
         for j, item in enumerate(tracks):
             frames_ids, positions, class_id = item['frame_ids'], item['positions'], item['class_id']
             if idx not in frames_ids:
@@ -133,31 +142,32 @@ def get_visual_data(save_dir, data_dir, video_dir, video_name, resnext101, devic
             else:
                 ptr = frames_ids.index(idx)
                 position = positions[ptr]
-                position = process_pos(position, im_width, im_height)
+                position = process_pos(position, im_width, im_height, new_width, new_height)
                 x1, y1, x2, y2 = position[0], position[1], position[2], position[3]
-                bbox = torch.FloatTensor([x1, y1, x2, y2]).unsqueeze(dim=0)
+                bbox = torch.FloatTensor([x1, y1, x2, y2]).unsqueeze(dim=0).to(device)
                 roi_feature = roi_align(feature_map, [bbox])
                 assert roi_feature.shape == (1, resnext_dim, pool_size, pool_size), \
                     'expected roi_feature.shape is  {} but got {}'.\
                     format((1, resnext_dim, pool_size, pool_size), roi_feature.shape)
 
                 objects_mask[i, j] = 1
-                temp_store.append(roi_feature)
+                temp_store.append(roi_feature.detach().cpu().numpy())
 
-        temp_store = torch.cat(temp_store, dim=0)
+        temp_store = np.concatenate([item for item in temp_store], axis=0)
         assert temp_store.shape == (num_tracks, resnext_dim, pool_size, pool_size), \
             'expected temp_store.shape == {}, but got {}'.\
                 format((num_tracks, resnext_dim, pool_size, pool_size), temp_store.shape)
         result_feature.append(temp_store)
 
-    result_feature = torch.cat([item.unsqueeze(dim=0) for item in result_feature], dim=0)
+    result_feature = np.concatenate([item[np.newaxis, ...] for item in result_feature], axis=0)
     assert result_feature.shape == (time_span, num_tracks, resnext_dim, pool_size, pool_size), \
         'expected result_feature.shape == {}, but got {}'.\
             format((time_span, num_tracks, resnext_dim, pool_size, pool_size), result_feature.shape)
 
     np.save(visual_feature_path, result_feature)
     np.save(class_feature_path, class_store)
-    np.save(objects_mask_path, objects_mask)
+    with open(objects_mask_path, 'wb') as f:
+        pickle.dump(objects_mask, f)
     with open(hastracks_path, 'wb') as f:
         pickle.dump(has_tracks, f)
 
